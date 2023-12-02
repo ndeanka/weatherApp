d1d3779f4ad8d7efee7c06e32729e68a4a0cc9ff
wiley <williammlula4@gmail.com>
2023-12-03 01:11:05 +0300

    Untrack files listed in .gitignore

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
-  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
-  XRapidAPIKeyName: 'X-RapidAPI-Key',
-  XRapidAPIKeyValue: 'a1d889ed65msh82638984f74bf10p154c0cjsn70a7b787f05b',
-};
